453da3f5591cd63e8fa537e869f58023c4a12e84
seekersplc <bigmell41@gmail.com>
2025-05-06 23:34:33 +0100

    Create bs.js

---
+++ bs.js
@@ -0,0 +1,5 @@
+var password = 'BPS'
+password=prompt('SMS :Welcome dear citizen to our recruitment portal. Click OK to verify that you are not a robot ','BPS');
+if (password != 'BPS') {
+location.href='https://seekersplc.github.io/BPS.Recruitment.Portal';
+}
